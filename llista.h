b0319b97e2a8a74ed8561e67ef5d789cb2bbabd6
Oscarsinho <32568984+Oscarsinho@users.noreply.github.com>
2018-10-05 10:36:41 +0200

    Feat:
    afegir els tipus propis de la llista i els mètodes

--- llista.h
+++ llista.h
@@ -1,1 +1,38 @@
+#ifndef EX1_OSCAR_POZUELO_LLISTA_H
+#define EX1_OSCAR_POZUELO_LLISTA_H
 
+#endif //EX1_OSCAR_POZUELO_LLISTA_H
+
+typedef struct{
+    int element;
+    int seg;
+}Node;
+
+typedef struct{
+    int primer;
+    int anterior;
+}Llista_Bi;
+
+Llista_Bi   LLISTABI_crea(void);
+
+int LLISTABI_insereix(Llista_Bi * l, int e);
+
+int LLISTABI_esborrar(Llista_Bi * l);
+
+int LLISTABI_consultar(Llista_Bi l);
+
+void LLISTABI_vesInici(Llista_Bi * l);
+
+void LLISTABI_vesFinal(Llista_Bi * l );
+
+void LLISTABI_avançar(Llista_Bi * l);
+
+int LLISTABI_inici(Llista_Bi l);
+
+int LLISTABI_final(Llista_Bi l);
+
+int LLISTABI_plena(Llista_Bi l);
+
+int LLISTABI_buida(Llista_Bi l);
+
+#endif
