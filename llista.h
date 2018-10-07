988f45ada621c85786968ad3b94ec66ac2153ede
Oscarsinho <32568984+Oscarsinho@users.noreply.github.com>
2018-10-07 19:21:16 +0200

    Feat:
    afegir mètodes

--- llista.h
+++ llista.h
@@ -13,13 +13,14 @@ typedef struct{
     int primer;
     int ultim;
     int cursor;
+    Node nodes[10];
 }Llista_Bi;
 
 Llista_Bi   LLISTABI_crea(void);
 
-int LLISTABI_insereix(Llista_Bi * l, int e);
+void LLISTABI_insereix(Llista_Bi * l, int e);
 
-int LLISTABI_esborrar(Llista_Bi * l);
+void LLISTABI_esborrar(Llista_Bi * l);
 
 int LLISTABI_consultar(Llista_Bi l);
 
@@ -27,7 +28,9 @@ void LLISTABI_vesInici(Llista_Bi * l);
 
 void LLISTABI_vesFinal(Llista_Bi * l );
 
-void LLISTABI_avançar(Llista_Bi * l);
+void LLISTABI_avancar(Llista_Bi * l);
+
+void LLISTABI_retrocedir(Llista_Bi * l);
 
 int LLISTABI_inici(Llista_Bi l);
 
@@ -36,5 +39,3 @@ int LLISTABI_final(Llista_Bi l);
 int LLISTABI_plena(Llista_Bi l);
 
 int LLISTABI_buida(Llista_Bi l);
-
-#endif
