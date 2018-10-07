78d8382431d2f47bc86d7794581685480d4098dd
Oscarsinho <32568984+Oscarsinho@users.noreply.github.com>
2018-10-07 19:53:41 +0200

    Test:
    afegir codi per comprovar que el programa funciona correctament

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
-
+#include "llista.h"
 int main() {
-    printf("Hello, World!\n");
+    Llista_Bi l;
+    int e;
+    l = LLISTABI_crea();
+    LLISTABI_insereix(&l,2);
+    e = LLISTABI_consultar(l);
+    printf("%d\n",e);
+    LLISTABI_insereix(&l,4);
+    LLISTABI_insereix(&l,1);
+    LLISTABI_avancar(&l);
+    LLISTABI_avancar(&l);
+    e = LLISTABI_consultar(l);
+    printf("%d\n",e);
+    LLISTABI_vesInici(&l);
+    e = LLISTABI_consultar(l);
+    printf("%d\n",e);
+
     return 0;
 }
